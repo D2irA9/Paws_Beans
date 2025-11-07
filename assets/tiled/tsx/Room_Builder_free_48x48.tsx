91f7d5f9ac90091ac8961a754509739810526909
D2irA9 <anastasiarpdaeva@gmail.com>
2025-11-07 23:52:08 +0300

    07/11/25 HI

---
+++ assets/tiled/tsx/Room_Builder_free_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Room_Builder_free_48x48" tilewidth="48" tileheight="48" tilecount="391" columns="17">
+ <image source="../../sprites/interior/Room_Builder_free_48x48.png" width="816" height="1104"/>
+</tileset>
