1d7ea2509e599df8e7603ecb4b77a8dee48974af
D2irA9 <anastasiarpdaeva@gmail.com>
2025-11-09 17:11:57 +0300

    Отрисовка станции

--- game.py
+++ game.py
@@ -3,38 +3,49 @@ from classes.tiled import Map
 from classes.character import Player
 from classes.node import Button
 
-# # # Переменные
+# # # Карты
 coffee_house = Map('assets/tiled/tmx/coffee_house.tmx', 48, 1)
 stations = Map('assets/tiled/tmx/stations.tmx', 48, 1)
 map_loaded = False # Флаг для проверки загрузки карт
-condition = 'coffee_house'
 
+# Все состояния
+condition = [
+    'coffee_house',
+    'station 1',
+    'station 2',
+    'station 3',
+]
+current_status = condition[0]
+# # # Игрок
 player = Player(3, (530, 60))
 
+# # # Узлы
 start  = Button(300, 100, ORANGE, (900, 620))
 
 def game(events):
-    global map_loaded, condition
+    global map_loaded, current_status
 
     for event in events:
         if event.type == py.QUIT:
             py.quit()
             sys.exit()
-
-        if event.type == py.MOUSEBUTTONDOWN:
+        elif event.type == py.MOUSEBUTTONDOWN:
             if start.signal(event.pos):
-                print('Сигнал получен ')
-                condition = 'stations'
+                if current_status == condition[0]:
+                    current_status = condition[1]
+                elif current_status == condition[1]:
+                    current_status = condition[0]
 
     if not map_loaded:
         coffee_house.load_map()
         stations.load_map()
         map_loaded = True
 
-    if condition == 'coffee_house':
+    if current_status == condition[0]:
         coffee_house.draw(screen)
         player.update()
         player.draw(screen)
-    else:
+    elif current_status == condition[1]:
         stations.draw(screen)
+
     start.draw(screen)
