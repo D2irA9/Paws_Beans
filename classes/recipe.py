28fa4040ed4f01349bdfcf875ebf0379598fa4e8
D2irA9 <anastasiarpdaeva@gmail.com>
2025-11-15 18:09:09 +0300

    Изменение экрана + 3 кнопки

---
+++ classes/recipe.py
@@ -0,0 +1,33 @@
+import pygame as py, random
+
+class Recipe:
+    """ Класс рецепты """
+
+    def __init__(self):
+        self.milk_types = ["обычное"] # "обезжиренное", "соевое"
+        self.espresso_types = ["кофе"] # "городское", "без кофеина"
+        self.cup_sizes = ["S", "M", "L"]
+
+    def generate_random_order(self):
+        """Генерирует случайный заказ"""
+        cup = random.choice(self.cup_sizes)
+        if cup == "S":
+            SE = random.randint(1, 4)
+            SM = 4 - SE
+        elif cup == "M":
+            SE = random.randint(1, 5)
+            SM = 5 - SE
+        else:
+            SE = random.randint(1, 6)
+            SM = 6 - SE
+
+        milk = random.choice(self.milk_types)
+        espresso = random.choice(self.espresso_types)
+
+        return {
+            "cup": cup,
+            "milk": milk,
+            "SM": SM,
+            "espresso": espresso,
+            "SE": SE
+        }
--- game.py
+++ game.py
@@ -2,13 +2,13 @@ from globals import *
 from classes.tiled import Map
 from classes.character import Player
 from classes.node import Button, CircleButton
+from  classes.recipe import Recipe
 
 # # # Карты
 coffee_house_map = Map('assets/tiled/tmx/coffee_house.tmx', 48, 1)
-
-stations1_map = Map('assets/tiled/tmx/stations1.tmx', 48, 1)
-stations2_map = Map('assets/tiled/tmx/stations2.tmx', 48, 1)
-stations3_map = Map('assets/tiled/tmx/stations3.tmx', 48, 1)
+# stations1_map = Map('assets/tiled/tmx/stations1.tmx', 48, 1)
+# stations2_map = Map('assets/tiled/tmx/stations2.tmx', 48, 1)
+# stations3_map = Map('assets/tiled/tmx/stations3.tmx', 48, 1)
 
 map_loaded = False # Флаг для проверки загрузки карт
 
@@ -24,16 +24,26 @@ current_status = condition[0]
 # # # Игрок
 player = Player(3, (530, 60))
 
+# # # Рецепт
+r = Recipe()
+order = r.generate_random_order()
+print(f"Чашка: {order['cup']}, Молоко: {order['milk']}({order["SM"]}), Эспрессо: {order['espresso']}({order["SE"]})")
+
 # # # Кнопки
+# Баковые кнопки
 button_left  = Button(300, 100, BLUE, (0, 620))
 button_left_S = False
 button_right  = Button(300, 100, ORANGE, (900, 620))
-
-circle_btn = CircleButton(35, ORANGE, (600, 350))
-circle_btn_S = False
+#
+circle_btn_S = CircleButton(35, ORANGE, (500, 350))
+circle_btn_S_signal = False
+circle_btn_M = CircleButton(35, GREEN, (600, 350))
+# circle_btn_M_signal = False
+circle_btn_L = CircleButton(35, VIOLET, (700, 350))
+# circle_btn_L_signal = False
 
 def game(events):
-    global map_loaded, current_status, button_left_S, circle_btn_S
+    global map_loaded, current_status, button_left_S, circle_btn_S_signal
 
     for event in events:
         if event.type == py.QUIT:
@@ -54,6 +64,13 @@ def game(events):
                 else:
                     current_status = condition[2]
 
+            if circle_btn_S.signal(event.pos):
+                print("!!!S")
+            elif circle_btn_M.signal(event.pos):
+                print("!!!M")
+            elif circle_btn_L.signal(event.pos):
+                print("!!!L")
+
             if button_left.signal(event.pos):
                 # Если stations1
                 if current_status == condition[1]:
@@ -65,15 +82,15 @@ def game(events):
     # Проверка загружена ли карта
     if not map_loaded:
         coffee_house_map.load_map()
-        stations1_map.load_map()
-        stations2_map.load_map()
-        stations3_map.load_map()
+        # stations1_map.load_map()
+        # stations2_map.load_map()
+        # stations3_map.load_map()
         map_loaded = True
 
     # Если coffee_house
     if current_status == condition[0]:
         button_left_S = False
-        circle_btn_S = False
+        circle_btn_S_signal = False
         coffee_house_map.draw(screen)
         player.update()
         player.draw(screen)
@@ -82,22 +99,25 @@ def game(events):
     # Если stations1
     elif current_status == condition[1]:
         button_left_S = True
-        circle_btn_S = True
-        stations1_map.draw(screen)
+        circle_btn_S_signal = True
+        # stations1_map.draw(screen)
+        screen.fill(ORANGE1)
         button_right.change_color(GREEN)
         button_left.change_color(BLUE)
     # Если stations2
     elif current_status == condition[2]:
         button_left_S = True
-        circle_btn_S = True
-        stations2_map.draw(screen)
+        circle_btn_S_signal = True
+        # stations2_map.draw(screen)
+        screen.fill(BLUE1)
         button_right.change_color(ORANGE)
         button_left.change_color(VIOLET)
     # Если stations3
     else:
         button_left_S = False
-        circle_btn_S = False
-        stations3_map.draw(screen)
+        circle_btn_S_signal = False
+        # stations3_map.draw(screen)
+        screen.fill(VIOLET1)
         button_right.change_color(BLUE)
 
     # Прорисовка кнопок
@@ -105,6 +125,8 @@ def game(events):
     if button_left_S:
         button_left.draw(screen)
     # Прорисовка круглой кнопки
-    if circle_btn_S:
-        circle_btn.draw(screen)
+    if circle_btn_S_signal:
+        circle_btn_S.draw(screen)
+        circle_btn_M.draw(screen)
+        circle_btn_L.draw(screen)
 
