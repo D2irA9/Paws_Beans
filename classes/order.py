f53795a029f4e5e96cad66293f93ecc1bb5a44d5
D2irA9 <anastasiarpdaeva@gmail.com>
2026-01-29 22:42:21 +0300

    генерация случайного заказа

--- classes/order.py
+++ classes/order.py
@@ -5,8 +5,33 @@ class Order:
     def __init__(self):
         self.milk = ['SKIM_MILK', 'STRAWBERRY_MILK']
         self.espresso = ['CITY_ROAST', 'DECAF_ROAST']
-        self.cups = ['S', 'M', 'L']
+        self.cup_capacity = {'S': 6, 'M': 5, 'L': 4}
         self.syrups = ['CHOCOLATE', 'RED_VELVET', 'SALTED_CARAMEL', 'SUGARPLUM']
 
     def generate_random_order(self):
         """Генерирует случайный заказ"""
+
+        # Выбираем стакан
+        cup = random.choice(['S', 'M', 'L'])
+        capacity = self.cup_capacity[cup]
+
+        # Максимальное количество эспрессо
+        max_esp = max(1, capacity - 2)
+
+        # Количество эспрессо
+        esp_qty = random.randint(1, max_esp)
+
+        # Создаем заказ
+        return {
+            'cup': cup,
+            'cup_capacity': capacity,
+            'espresso': {
+                'type': random.choice(self.espresso),
+                'portions': esp_qty
+            },
+            'milk': {
+                'type': random.choice(self.milk),
+                'portions': capacity - esp_qty
+            },
+            'syrup': random.choice(self.syrups)
+        }
